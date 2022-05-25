cfbb298b8d04376a83c780ed34416626597cf5a4
Albert Garcia <alberd_beto@hotmail.com>
2022-05-25 03:14:33 +0200

    app routering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-import Upload from "./pages/Uploadd.jsx";
-import Home from "./pages/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home.jsx";
 import Navbar from "./components/navbar/navbar";
+import Login from "./components/Login/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
+import SignUp from "./components/SignUp/SignUp";
+import Search from "./pages/Search/Search";
+import Upload from "./pages/Upload/Upload";
+
 function App() {
   return (
-    <div className="container">
+    <div className="">
       <Router>
         <Navbar />
-        <nav className="nav">
-          <div className="nav-brand">Gifone</div>
-          <ul className="nav-items">
-            <li className="nav-item">
-              <Link to="/">Gallery</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/upload">Upload</Link>
-            </li>
-          </ul>
-        </nav>
+
         <Routes>
-          <Route element={<Upload />} path="/upload" />
+          <Route element={<ProtectedRoutes />} path="/user/*" />
           <Route element={<Home />} path="/" />
+          <Route element={<SignUp />} path="/signUp" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Search />} path="/search" />
+          <Route path="/upload" element={<Upload />} />
         </Routes>
       </Router>
     </div>
