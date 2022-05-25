2db616f2650701b7549864f61d3a4bcbb9955d0a
Albert Garcia <alberd_beto@hotmail.com>
2022-05-25 03:14:17 +0200

    server config add

---
+++ server/src/config/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  port: process.env.PORT,
+  firebase: {
+    certConfig: {
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_APP_ID,
+    },
+  },
+};
+
+export default config;
